bffc2f6effbeb92beeed490104ddfa07c80a3779
Dave Manley <manley@stanfordalumni.org>
2016-01-19 15:01:26 +0000

    initial timestamp code

--- server.js
+++ server.js
@@ -8,26 +8,38 @@ var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+function getTime(str) {
+    let timestamp = Date.parse(str);
+    let num = parseInt(str);
+    let num2 = Number(str);
+    let retval = {
+        "unix": null,
+        "natural": null
+    };
+    if (num == num2) {
+        let date = new Date(num);
+        retval["unix"] = num;
+        retval["natural"] = date.toDateString();
+    } else if (isNaN(timestamp) == false) {
+        let date = new Date(str);
+        retval["natural"] = date.toDateString();
+        retval["unix"] = date.getTime();
+    }
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+    console.log(retval);
+    return JSON.stringify(retval);
+}
+
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.use('/', express.static(process.cwd() + '/public'));
+app.get('/:time', function(req, res) {
+    let s = req.params.time;
+    let retval = getTime(s);
+    res.end(retval);
+})
 
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port + '...');
 });
